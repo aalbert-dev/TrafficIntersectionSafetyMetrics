554ee2116ec12616c829daf3c5bfabd666a41ed3
Arjun <arjunalbert1@gmail.com>
2024-07-28 15:13:42 -0400

    added visualization

--- Sim.py
+++ Sim.py
@@ -1,11 +1,12 @@
 from Vehicle import *
 from Pose import Pose
 import numpy as np
+from Viz import Visualizer
 
-TIME_HORIZON = 10
+TIME_HORIZON = 20
 DT = 1
 
-def simulate(vehicle: Vehicle):
+def simulate(vehicle: Vehicle) -> list[Pose]:
     current_time = 0
     pose_list = []
     while current_time < TIME_HORIZON:
@@ -15,34 +16,60 @@ def simulate(vehicle: Vehicle):
     return pose_list
 
 
-def find_slope_intercept(vehicle: Vehicle):
-   m = np.tan(vehicle.pose.heading)
-   x = vehicle.pose.x
-   y = vehicle.pose.y
-   b = y - m * x
-   return m, b
+def find_slope_intercept(vehicle: Vehicle) -> tuple[float, float]:
+    m = np.tan(vehicle.pose.heading)
+    x = vehicle.pose.x
+    y = vehicle.pose.y
+    b = y - m * x
+    return m, b
 
 
-def find_intersection(m1: float, b1: float, m2: float, b2: float):
+def find_intersection(m1: float, b1: float, m2: float, b2: float) -> Pose:
 
-  if m1 == m2:
-    return None
+    if m1 == m2:
+        return None
 
-  x = (b2 - b1) / (m1 - m2)
-  y = m1 * x + b1
-  return x, y
+    x = (b2 - b1) / (m1 - m2)
+    y = m1 * x + b1
+    return Pose(x, y, 0)
 
 
-def find_intersection_vehicles(vehicle_a: Vehicle, vehicle_b: Vehicle):
-   m1, b1 = find_slope_intercept(vehicle_a)
-   m2, b2 = find_slope_intercept(vehicle_b)
-   return find_intersection(m1, b1, m2, b2)
+def find_linear_intersection_vehicles(vehicle_a: Vehicle, vehicle_b: Vehicle) -> Pose:
+    m1, b1 = find_slope_intercept(vehicle_a)
+    m2, b2 = find_slope_intercept(vehicle_b)
+    return find_intersection(m1, b1, m2, b2)
 
+
+def find_quadratic_intersection_vehicles(vehicle_a: Vehicle, vehicle_b: Vehicle) -> Pose:
+    m1, b1 = find_slope_intercept(vehicle_a)
+    m2, b2 = find_slope_intercept(vehicle_b)
+    return find_intersection(m1, b1, m2, b2)
+
+
+def distance(p1: Pose, p2: Pose) -> float:
+    return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+
+
+def convert_poses_to_distance_to_intersection(pose_list: list[Pose], intersection: Pose) -> list[float]:
+    return [distance(pose, intersection) for pose in pose_list]
 
 vehicle_a = Vehicle(Pose(0, 0, 0), 1, 0)
-vehicle_b = Vehicle(Pose(0, -2, np.pi / 4), 1, 0)
+vehicle_b = Vehicle(Pose(0, -20, np.pi / 4), 0, 2)
 
 
-result = find_intersection_vehicles(vehicle_a, vehicle_b)
+intersection = find_linear_intersection_vehicles(vehicle_a, vehicle_b)
 
-print(result)
+pose_list_a = simulate(vehicle_a)
+pose_list_b = simulate(vehicle_b)
+
+distance_list_a = convert_poses_to_distance_to_intersection(pose_list_a, intersection)
+
+distance_list_b = convert_poses_to_distance_to_intersection(pose_list_b, intersection)
+
+time_list = list(np.linspace(0, TIME_HORIZON, int(TIME_HORIZON / DT)))
+
+viz = Visualizer(None)
+
+viz.add_plot(time_list, distance_list_a)
+viz.add_plot(time_list, distance_list_b)
+viz.display()
--- Viz.py
+++ Viz.py
@@ -1,7 +1,16 @@
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 class Visualizer:
     def __init__(self, vehicles):
         self.vehicles = vehicles
 
+
+    def add_plot(self, time_list, distance_list):
+        plt.plot(time_list, distance_list, 'r--')
+
     def display(self):
+        plt.axhline(0,color='black')
+        plt.axis((-20, 20, -20, 20))
+        plt.show()
